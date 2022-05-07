59be2e91a28c4d0addc49e6ef51c16833343aeb5
gsornsen <gsornsen@gmail.com>
2022-05-07 09:33:39 -0700

    Use nix without opening a new terminal

--- hooks/install.fish
+++ hooks/install.fish
@@ -3,6 +3,7 @@ if test curl
     echo "Fetching Setup Script!"
     curl https://raw.githubusercontent.com/lilyball/nix-env.fish/master/conf.d/nix-env.fish \
         > ~/.config/fish/conf.d/nix-env.fish
+    exec $SHELL
 else
     echo "Please install curl and try again!"
 end
